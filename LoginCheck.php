bcdc729f0183f59e3ccc8f194da7030c0b7c1735
tugrulokms <tugrul1997@hotmail.com>
2017-01-02 23:36:49 +0300

    update

--- LoginCheck.php
+++ LoginCheck.php
@@ -1,23 +1,15 @@
 <?php
-if (session_status() == PHP_SESSION_NONE)
-    session_start();
 include_once "Data/DatabaseConnection.php";
 $user = $_POST["username"];
 $password = md5($_POST["password"]);
-$login_check = $database->query('SELECT count(*) FROM users WHERE UserName = ' ."$user". ' AND Password = ' .$password);
-/*$login_check = $database->count("users",[
-    " AND " => [
-    "UserName" => $user,
-    "Password" => $password
-    ]
-]);*/
+$login_check = $database->query('SELECT count(*) FROM users WHERE UserName = ' ."$user". ' AND Password = ' ."$password");
 if($login_check === 1) {
+    session_start();
     $_SESSION["id"] = 1;
     ?>
     <script>
         window.location.href = "AccountPage.php";
     </script>
-
     <?php
 } else {
     ?>
